e3d06bb986c95da94c15e9bf19d9302d18efce6c
unknown <kaiserabir222@gmail.com>
2021-10-01 11:54:06 +0600

    updated inventory

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Inventory = () => {
     return (
         <div>
-            <h3>This is inventory</h3>
+            <h3>This is inventory Page</h3>
         </div>
     );
 };
